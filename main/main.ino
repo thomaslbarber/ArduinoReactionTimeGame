0612c3b56b82420c59d0f4c45f85bf1c01cee841
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-28 09:58:37 +0100

    Imported lcd library and added pin definitions.

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,19 @@
+// Includes...
+#include "LiquidCrystal.h"
+
+// Pin locations on the arduino board (for the display).
+#define BS_PIN 7
+#define E_PIN 8
+#define D4_PIN 9
+#define D5_PIN 10
+#define D6_PIN 11
+#define D7_PIN 12
+#define BTN_PIN 13
+
+// The display.
+LiquidCrystal lcd(BS_PIN, E_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
+
+
 void setup() {
   // put your setup code here, to run once:
 
